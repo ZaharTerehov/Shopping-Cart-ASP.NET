097315d52539586ed7b675b1ecc23dfeceeda371
z.terehov <zaharkuli67@gmail.com>
2022-11-24 11:36:25 +0300

    added migration

--- ShoppingCart/ShoppingCart/Program.cs
+++ ShoppingCart/ShoppingCart/Program.cs
@@ -3,10 +3,9 @@ using ShoppingCart.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-	options.UseSqlServer(builder.Configuration["ConnectionDb"]);
-});
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+	builder.Configuration.GetConnectionString("ConnectionDb")
+));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
