fd4cb83915b5860b84c3b1b01adec33400987ddd
z.terehov <zaharkuli67@gmail.com>
2022-11-25 16:27:01 +0300

    added categories view component

---
+++ ShoppingCart/ShoppingCart/Components/CategoriesViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Data;
+
+namespace ShoppingCart.Components
+{
+	public class CategoriesViewComponent : ViewComponent
+	{
+		private readonly ApplicationDbContext _context;
+
+		public CategoriesViewComponent(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync() => View(await _context.Categories.ToListAsync());
+	}
+}
