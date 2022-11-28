5b84c43f8f31eb15d6c09333ad7085a0a9712424
z.terehov <zaharkuli67@gmail.com>
2022-11-28 13:56:29 +0300

    added small cart view component

---
+++ ShoppingCart/ShoppingCart/Components/SmallCartViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Models;
+using ShoppingCart.Models.ViewModel;
+
+namespace ShoppingCart.Components
+{
+	public class SmallCartViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			List<CartItem> cartItems = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+			SmallCartViewModel smallCartVM;
+
+			if (cartItems == null || cartItems.Count == 0)
+				smallCartVM = null;
+			else
+			{
+				smallCartVM = new()
+				{
+					NumberOfItems = cartItems.Sum(x => x.Quantity),
+					TotalAmount = cartItems.Sum(x => x.Quantity * x.Price)
+				};
+			}
+
+			return View(smallCartVM);
+		}
+		
+	}
+}
---
+++ ShoppingCart/ShoppingCart/Models/ViewModel/SmallCartViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingCart.Models.ViewModel
+{
+	public class SmallCartViewModel
+	{
+		public int NumberOfItems { get; set; }
+
+		public decimal TotalAmount { get; set; }
+	}
+}
