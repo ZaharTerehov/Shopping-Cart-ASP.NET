6c61fecc348b53c4b5d47bcbd38ee4d88fa32d57
z.terehov <zaharkuli67@gmail.com>
2022-11-30 21:26:34 +0300

    added cart selected

--- ShoppingCart/ShoppingCart/Controllers/CartController.cs
+++ ShoppingCart/ShoppingCart/Controllers/CartController.cs
@@ -26,5 +26,25 @@ namespace ShoppingCart.Controllers
 
 			return View(cartVM);
 		}
-	}
+
+        public async Task<IActionResult> Add(Guid id)
+        {
+			Product product = await _context.Products.FindAsync(id);
+
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+
+			CartItem cartItem = cart.Where(c => c.ProductId == id).FirstOrDefault();
+
+			if(cartItem == null)
+				cart.Add(new CartItem(product));
+			else
+				cartItem.Quantity += 1;
+
+			HttpContext.Session.SetJson("Cart", cart);
+
+			TempData["Success"] = "The product has been added!";
+
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+    }
 }
